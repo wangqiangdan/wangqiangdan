0446eb9a20ffa843ba8a0123e7b9ba3d68753b16
wangqiangdan <wangqiangdan@58ganji.com>
2017-12-20 18:09:26 +0800

    git工具测试

--- meadowlark.js
+++ meadowlark.js
@@ -1,15 +1,9 @@
 var express = require('express'); // 加载express模块
-var app = express(); // 创建服务器实例
-var fortunes = [
-    '幸运1',
-    '幸运2',
-    '幸运3',
-    '幸运4',
-    '幸运5'
-];
 var handlebars = require('express3-handlebars').create({
     defaultLayout: 'main'
 });
+var fortune = require('./lib/fortune.js');
+var app = express(); // 创建服务器实例
 app.engine('handlebars', handlebars.engine); // 将express模板引擎配置成handlebars 
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000); // 配置全局变量 post
@@ -18,9 +12,8 @@ app.get('/', function (req, res) {
     res.render('home');
 })
 app.get('/about', function (req, res) {
-    var randomfortune = fortunes[parseInt(Math.random() * fortunes.length)];
     res.render('about', {
-        fortune: randomfortune
+        fortune: fortune.getFortunes()
     });
 });
 // 定制 404 页面
